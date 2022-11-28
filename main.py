3c1293f8f003d5b36dd29e46b2ebc2c5637fef50
aadithyajp <97959379+aadithyajp@users.noreply.github.com>
2022-11-28 14:15:59 +0530

    stage before data ingestion

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
-
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
+def test_exception():
+    try:
+        logging.info("we are dividing be 0")
+        x=65656/0
+    except Exception as e:
+        raise SensorException(e,sys)
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
-    
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+    # mongodb_client = MongoDBClient()
+    # print(mongodb_client.database.list_collection_names())
---
+++ sensor/constant/s3_bucket.py
@@ -0,0 +1,2 @@
+TRAINING_BUCKET_NAME = "sensor-model"
+PREDICTOIN_BUCKET_NAME = "sensor-datasource"
