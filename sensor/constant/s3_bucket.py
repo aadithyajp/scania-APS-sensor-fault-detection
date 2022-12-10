0bbb14847831094bed64aadc4ca0f6060f2b2230
aadithyajp <97959379+aadithyajp@users.noreply.github.com>
2022-12-10 11:52:20 +0530

    ready for deployment

--- sensor/constant/s3_bucket.py
+++ sensor/constant/s3_bucket.py
@@ -1,2 +1,2 @@
-TRAINING_BUCKET_NAME = "sensor-model"
+TRAINING_BUCKET_NAME = "scania-sensor-model-pipeline"
 PREDICTOIN_BUCKET_NAME = "sensor-datasource"
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -6,7 +6,9 @@ from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
 from sensor.components.model_evaluation import ModelEvaluation
 from sensor.components.model_pusher import ModelPusher
-
+from sensor.cloud_storage.s3_syncer import S3Sync
+from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR
 # from sensor.components
 from sensor.data_access.sensor_data import SensorData
 from sensor.logger import logging
@@ -14,8 +16,11 @@ from sensor.exception import SensorException
 import sys,os
 class TrainPipeline:
     
+    is_pipeline_running=False
+    
     def __init__(self):
        self.Training_Pipeline_Config = TrainingPipelineConfig()
+       self.s3_sync = S3Sync()
         # self.Data_Ingestion_Config = DataIngestionConfig(Training_Pipeline_Config)
         # self.Training_Pipeline_Config = Training_Pipeline_Config
         
@@ -86,8 +91,28 @@ class TrainPipeline:
         except  Exception as e:
             raise  SensorException(e,sys)
         
+    
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_buket_url=aws_buket_url)
+        except Exception as e:
+            raise SensorException(e,sys)
+            
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
+            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_buket_url=aws_buket_url)
+        except Exception as e:
+            raise SensorException(e,sys)
+        
+        
+        
     def run_pipeline(self):
         try:
+             
+            TrainPipeline.is_pipeline_running=True
+             
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
@@ -96,5 +121,10 @@ class TrainPipeline:
             if not model_eval_artifact.is_model_accepted:
                 raise Exception("Trained model is not better than the best model")
             model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
+            TrainPipeline.is_pipeline_running=False
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
         except Exception as e:
+            self.sync_artifact_dir_to_s3()
+            TrainPipeline.is_pipeline_running=False
             raise SensorException(e,sys)
